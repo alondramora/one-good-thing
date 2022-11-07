67eb220c9b8d878e0a7058aab0d4b32dd02de004
alondramora <mora_ac@yahoo.com>
2022-11-06 21:11:39 -0800

    installed daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 module.exports = {
   content: ['./views/**/*.{ejs, .html}'],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          
+          "primary": "#545CFC",
+                   
+          "secondary": "#7E5EF1",
+                   
+          "accent": "#F7839A",
+                   
+          "neutral": "#1C192A",
+                   
+          "base-100": "#FFFFFF",
+                   
+          "info": "#F2A75B",
+                   
+          "success": "#a3e635",
+                   
+          "warning": "#fbbf24",
+                   
+          "error": "#FC4A86",
+          },
+        },
+    ],
+  },
 }
