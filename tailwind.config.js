9956ea2379f28db14e2ba426974291ef4b9cc48f
alondramora <mora_ac@yahoo.com>
2022-11-12 20:06:29 -0800

    changed color of buttons on index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px',
-      },
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
@@ -18,21 +18,21 @@ module.exports = {
         mytheme: {
           
           "primary": "#545CFC",
-                   
+
           "secondary": "#7E5EF1",
-                   
+
           "accent": "#F7839A",
-                   
+
           "neutral": "#1C192A",
-                   
+
           "base-100": "#FFFFFF",
-                   
+
           "info": "#F2A75B",
-                   
+
           "success": "#a3e635",
-                   
+
           "warning": "#fbbf24",
-                   
+
           "error": "#FC4A86",
           },
         },
